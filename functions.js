2da0d4cc889ebb55964c6ee7d50cac65190f7eeb
Kyrin <kyrinkompi@gmail.com>
2025-10-30 02:25:19 +0200

    Beta 1.4
    - Added new server API route
    - Added a secondary default video (360p)
    - Fixed bug
    - Increased max video requirement to 5GB
    - Organized and slightly refactored code

--- functions.js
+++ functions.js
@@ -228,12 +228,6 @@ async function fetchVideo() {
         const progressText = document.getElementById("progress-txt");
         const progress = document.querySelector(".progress");
         downloadBtn.onclick = async () => {
-            const size = parseInt(document.getElementById("v-size").innerText);
-            if (size >= 5000) {
-                progressText.innerText = "Woahhh you can't download a video that's 5GB or larger just yet. Try lower quality";
-                return
-            }
-
             downloadBtn.disabled = true;
             downloadBtn.style.filter = "brightness(90%)";
             progress.style.width = "0%";
@@ -248,6 +242,12 @@ async function fetchVideo() {
                         downloadBtn.style.filter = "brightness(100%)";
                     }, 4450)
                 } else if (format === "video") {
+                    const size = parseInt(document.getElementById("v-size").innerText);
+                    if (size >= 5000) {
+                        progressText.innerText = "Woahhh you can't download a video that's 5GB or larger just yet. Try lower quality";
+                        return
+                    }
+
                     const ws = new WebSocket('wss://yougra-server.onrender.com');
                     let processed = false;
 
